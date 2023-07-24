bba054ac1d326f7982400027c1d190c704ee9357
assegidasefa <assegidassefa7@gmail.com>
2023-07-24 11:01:21 +0300

    adding new crud operation

--- routes/newsRoute.js
+++ routes/newsRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 const newsRouter = express.Router()
 
-import { createNews, getNews, updateNews } from '../controllers/newsController.js'
+import { createNews, deleteNews, getNews, updateNews } from '../controllers/newsController.js'
 
 newsRouter.post("/",createNews)
 newsRouter.get("/:id",getNews)
 newsRouter.put("/:id",updateNews)
+newsRouter.delete("/:id",deleteNews)
 
 
 
